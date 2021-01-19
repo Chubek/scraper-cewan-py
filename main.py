adbfe75f33d7a5f80fcb9278897184003adfed44
Chubak <chubakbidpaa@gmail.com>
2021-01-19 06:34:30 +0330

    fixed error

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-from trigger import *
 from db import Database
-
+import logging
+import threading
 
 if __name__ == "__main__":
+
+    database = Database("mongo://chubak:4d4m4k:Dummy@localhost:27017/", "articles_db", "articles_coll")
+
+    functions = [database.insert_abc, 
+                        database.insert_bbc,
+                        database.insert_cbs, 
+                        database.insert_cnn, 
+                        database.insert_fox, 
+                        database.insert_utd, 
+                        database.insert_nbc]
+
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    threads = list()
+    for func in functions:
+        logging.info("Main    : create and start thread %d.", str(func))
+        x = threading.Thread(target=lambda x: x(), args=(func,))
+        threads.append(x)
+        x.start()
+
+    for index, thread in enumerate(threads):
+        logging.info("Main    : before joining thread %d.", index)
+        thread.join()
+        logging.info("Main    : thread %d done", index)
     
